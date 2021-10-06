3639108993dc3c0ff359cae7b8ffa28010440d81
rutikeyone <rutikeyone@gmail.com>
2021-10-06 21:39:01 +0300

    Add library and begin layout

---
+++ app/src/main/java/com/tvshowapp/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tvshowapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tvshowapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/tvshowapp/models/TvShow.java
@@ -0,0 +1,10 @@
+package com.tvshowapp.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class TvShow {
+
+    @SerializedName("id")
+    private int id;
+
+}
